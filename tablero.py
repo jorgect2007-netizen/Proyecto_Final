4e2a2f77ae6ae3a579c5dd08ad988b7a75a249a5
Alex3758b <bazdiazalejandro@gmail.com>
2025-11-27 18:24:56 +0100

    Implement Luigi character and controls
    Added Luigi character and movement controls.

--- tablero.py
+++ tablero.py
@@ -15,11 +15,30 @@ class Tablero:
         pyxel.run(self.update, self.draw)
         pyxel.load("assets/resources.pyxres")
 
+  self.mario = Personaje(x=None, y=None, sprites= mario_sprites, nivel=0,
+                     tope_arriba=2, tope_abajo=0)
+        self.luigi = Personaje(x=None, y=None, sprites= luigi_sprites , 
+                               nivel=1,
+                     tope_arriba=2, tope_abajo=0)
+
     def update(self):
+        if pyxel.btn(pyxel.KEY_UP):
+            self.mario.mover("arriba")
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.mario.mover("abajo")
 
+        if pyxel.btn(pyxel.KEY_W):
+            self.luigi.mover("arriba")
+        if pyxel.btn(pyxel.KEY_S):
+            self.luigi.mover("abajo")
+
+        self.mario.update()
+        self.luigi.update()
         pass
-
-    def draw(self):
+        
+        def draw(self):
         pyxel.cls(7)
+        self.mario.draw()
+        self.luigi.draw()
 
 prueba = Tablero(256*2+16, 256)
