c1c6bb555fb0657022583a4c49b44eb47e4ca73a
jorgect2007-netizen <jorgect2007@gmail.com>
2025-11-27 17:45:52 +0100

    Actualización a las 17:45

--- clases.py
+++ clases.py
@@ -9,7 +9,11 @@ class Personaje:
         self.nivel = nivel
         self.tope_arriba = tope_arriba
         self.tope_abajo = tope_abajo
-
+    sprites_mario = {
+        'abajo':(2,0,0,16,16),
+        'arriba':(2,0,0,16,16),
+        'triste':(2,0,0,16,16)
+    }
     @property
     def x(self) -> int:
         return self.__x
@@ -117,38 +121,6 @@ class Paquete:
             self.__y = y
 
 
-class Cinta:
-    def __init__(self, x: int, y: int, sprites: tuple, velocidad: float):
-        self.x = x
-        self.y = y
-        self.sprites = None
-        self.velocidad = velocidad
-
-    @property
-    def x(self) -> int:
-        return self.__x
-
-    @property
-    def y(self) -> int:
-        return self.__y
-
-    @x.setter
-    def x(self, x: int):
-        if not isinstance(x, int):
-            raise TypeError("La x debe ser un entero " + str(type(x)))
-        elif x < 0:
-            raise ValueError("La x no debe ser un número positivo")
-        else:
-            self.__x = x
-
-    @y.setter
-    def y(self, y: int):
-        if not isinstance(y, int):
-            raise TypeError("La y debe ser un entero " + str(type(y)))
-        elif y < 0:
-            raise ValueError("La y debe ser un número positivo")
-        else:
-            self.__y = y
 
 class Jefe:
     def __init__(self, x: int, y: int, sprites: tuple, enfadado: bool):
--- tablero.py
+++ tablero.py
@@ -1,7 +1,25 @@
 from clases import Personaje
 from clases import Camion
 from clases import Paquete
-from clases import Cinta
 from clases import Jefe
 import pyxel
 
+class Tablero:
+
+    def __init__(self, ancho: int, alto: int):
+
+        self.ancho = ancho
+        self.alto = alto
+
+        pyxel.init(self.ancho, self.alto, title="Demo Juego Mario Bros")
+        pyxel.run(self.update, self.draw)
+        pyxel.load("assets/resources.pyxres")
+
+    def update(self):
+
+        pass
+
+    def draw(self):
+        pyxel.cls(7)
+
+prueba = Tablero(256*2+16, 256)
